34ed4cea60d2c659fd08174a2688d3873f321bc9
acmno123 <153980590+acmno123@users.noreply.github.com>
2025-10-16 19:41:23 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Features from './pages/Features'
-import Invite from './pages/Invite'
+import Docs from './pages/Docs'
+import Pricing from './pages/Pricing'
+import Gallery from './pages/Gallery'
 import About from './pages/About'
+import Invite from './pages/Invite'
 
-export default function App(){
+export default function App() {
   return (
     <BrowserRouter>
-      <div className='min-h-screen flex flex-col'>
+      <div className="flex flex-col min-h-screen">
         <Navbar />
-        <main className='flex-grow max-w-6xl mx-auto p-6 w-full'>
+        <main className="flex-grow max-w-6xl mx-auto p-6 w-full">
           <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/features' element={<Features/>} />
-            <Route path='/invite' element={<Invite/>} />
-            <Route path='/about' element={<About/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/docs" element={<Docs />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/invite" element={<Invite />} />
           </Routes>
         </main>
         <Footer />
